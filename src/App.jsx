e8360ee83cab9f70d0cc23dac00c9ae920c2ee54
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-04-06 13:29:04 +0100

    updated index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Tech, Works, StarsCanvas, Navbar } from "./components";
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      3D Developer Portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div relative z-0>
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
