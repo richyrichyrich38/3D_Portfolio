d2e9f31c92dddf6c3d11b4f609dbbe166cdb0aef
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-04-07 11:38:19 +0100

    navbar build complete large and small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to='/' className="flex items-center gap-2"
         onClick={() => {
@@ -19,13 +20,40 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">Richard Hills Jr <span className="sm:block hidden">| Web Developer</span></p>
         </Link>
+        {/* navbar when large screen */}
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
-            <li>
+            <li key={link.id} className={`${
+              active === link.title ? 'text-white' : 'text-secondary'
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={() => setActive(link.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
+        {/* Menu hamburger */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}/>
+
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+              {/* nav links for hamburger menu */}
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+          {navLinks.map((link) => (
+            <li key={link.id} className={`${
+              active === link.title ? 'text-white' : 'text-secondary'
+            } font-poppins font-medium cursor-pointer text-[16px]`}
+            // closes with close click or link click
+            onClick={() => {
+              setToggle(!toggle);
+              setActive(link.title);
+              }}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+            </div>
+        </div>
       </div>
     </nav>
   )
