85703c82dccf029e4c971a51ccab1f6f3aa2f36d
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-04-11 11:16:16 +0100

    fixed 3d load. i was exporting Computers rather than ComputersCanvas. adjusted 3d model for mobile devices. Hero section now complete

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from '../Loader';
 import { Mesh } from 'three';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
@@ -21,14 +21,35 @@ const Computers = () => {
       shadow-mapSize={1024}
       />
       <primitive object={computer.scene}
-      scale={0.75}
-      position={[0, -3.25, -1.5]}
+      scale={isMobile ? 0.7 : 0.75}
+      position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
       rotation={[-0.01, -0.2, -0.1]} />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+
+  }, [])
+
+
+
   return (
   <Canvas 
   frameLoop='demand' 
@@ -42,7 +63,7 @@ const ComputersCanvas = () => {
       maxPolarAngle={Math.PI / 2}
       minPolarAngle={Math.PI / 2}
       />
-      <Computers />
+      <Computers isMobile={isMobile} />
     </Suspense>
 
     <Preload all />
@@ -50,4 +71,4 @@ const ComputersCanvas = () => {
   </Canvas>)
 }
 
-export default Computers
+export default ComputersCanvas
