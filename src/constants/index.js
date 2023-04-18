8a787b9d93c34e633f4feedd8fcfed6163d82ac0
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-04-18 11:58:24 +0100

    updated testimonial

--- src/constants/index.js
+++ src/constants/index.js
@@ -182,27 +182,27 @@ const testimonials = [
   {
     testimonial:
       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+    name: "Nick Meldrum",
+    designation: "CEO",
+    company: "Who's Best Group",
+    image: "https://media.licdn.com/dms/image/C4D03AQFdLo8ewY7y2w/profile-displayphoto-shrink_800_800/0/1597225209962?e=1687392000&v=beta&t=cIsWOE16MZUUAU8nLTXAP_HOfkDnjRqgMuRSu5fDpPk",
   },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
+  // {
+  //   testimonial:
+  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //   name: "Chris Brown",
+  //   designation: "COO",
+  //   company: "DEF Corp",
+  //   image: "https://randomuser.me/api/portraits/men/5.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   name: "Lisa Wang",
+  //   designation: "CTO",
+  //   company: "456 Enterprises",
+  //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+  // },
 ];
 
 const projects = [
