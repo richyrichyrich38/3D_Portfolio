9524efb0bf64f2e3ac4904b9b14dcac7490766ac
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-07-27 13:16:46 +0100

    updated projects section description

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -82,7 +82,7 @@ const Works = () => {
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          links to the deployed sites via the web icon in the top right corner of each card. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
